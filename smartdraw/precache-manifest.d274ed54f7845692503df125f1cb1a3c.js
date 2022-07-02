c0bd8503ca2ecba6a514ebb85b90ea6de14f9dc2
jawei <1005945485@qq.com>
2022-07-02 10:15:58 +0800

    feat: weword & smartdraw

---
+++ smartdraw/precache-manifest.d274ed54f7845692503df125f1cb1a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db7a9fa5f466aee0557d",
+    "url": "/js/vendor.db7a9fa5.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "79737e022ac310e8492e",
+    "url": "/js/app.79737e02.js"
+  },
+  {
+    "revision": "0a0bbef13bad9eb559f52c5016f4b7dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea7b449278322c5fa0a049284b895eb4",
+    "url": "/img/draw/texture.png"
+  },
+  {
+    "revision": "79737e022ac310e8492e",
+    "url": "/css/app.8716f824.css"
+  }
+];
